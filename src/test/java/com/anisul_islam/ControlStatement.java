a815a4cc71404e4e2506b1dbac47d57927c6fa07
Ali Noor <ali.noor12ny@yahoo.com>
2023-01-24 11:23:22 -0500

    Second commite

---
+++ src/test/java/com/anisul_islam/ControlStatement.java
@@ -0,0 +1,33 @@
+package com.anisul_islam;
+
+import java.util.Scanner;
+
+public class ControlStatement {
+	
+	/*
+	 * it allow us to take the decision when to execute and what to be done to be
+	 * execute.
+	 */
+
+	public static void main(String[] args) {
+		
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number");
+		num=sc.nextInt();
+		
+		if (num>0 ) {
+			System.out.println("positive");
+			
+		}else if (num==0){
+			System.out.println("this is not a positive nor nagitive");
+		}else {
+			System.out.println("Nagitive0");
+		}
+		
+
+
+	}
+
+}
